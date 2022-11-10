b2302078f423b793cda1bde04a23513c0535249f
CHE <m.monteroapaza@gmail.com>
2022-11-09 22:31:14 -0500

    login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+
+    }
+
+    public function logout(Request $request)
+    {
+        $accessToken = $request->bearerToken();
+
+        // Get access token from database
+        $token = PersonalAccessToken::findToken($accessToken);
+
+        // Revoke token
+        $token->delete();
+        return response()->json(['message' => "Logged out successfully!"], 200);
+    }
+
+    public function user(Request $request)
+    {
+        return auth()->user();
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('posts')->group(function () {
     Route::get('/', [PostController::class, 'showPosts']);
     Route::get('{post}', [PostController::class, 'show']);
+    Route::delete('{post}', [PostController::class, 'delete']);
 });
 
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('user', [AuthController::class, 'user']);
+    Route::prefix('dashboard')-> group(function(){
+        Route::apiResource('categories', CategoryController::class);
+    });
+});
